f4390a51a1066dbb9b3ac3e41a8f4a9dfe88c991
Wurmatron <wurmatron@gmail.com>
2016-05-25 22:01:39 -0400

    Added 1 AE recipe to get feel of recipes for them

--- src/main/java/wurmcraft/wurmatron/common/recipes/mods/AERecipes.java
+++ src/main/java/wurmcraft/wurmatron/common/recipes/mods/AERecipes.java
@@ -1,9 +1,11 @@
 package wurmcraft.wurmatron.common.recipes.mods;
 
 import appeng.core.Api;
+import com.rwtema.extrautils.ExtraUtils;
 import cpw.mods.fml.common.Optional;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import wurmcraft.wurmatron.common.items.WurmTweaksItems;
 import wurmcraft.wurmatron.common.recipes.RecipeHelper;
 
 public class AERecipes {
@@ -15,5 +17,6 @@ public class AERecipes {
 				r.addCross(Api.INSTANCE.blocks().blockQuartzGlass.stack(4), Blocks.glass, Items.quartz);
 				r.addShapeless(Api.INSTANCE.blocks().blockQuartzVibrantGlass.stack(1), Api.INSTANCE.blocks().blockQuartzGlass.stack(1), Items.glowstone_dust);
 				r.addShapeless(Api.INSTANCE.blocks().blockQuartzTorch.stack(1), Items.quartz, Items.glowstone_dust);
+				r.addBasicMachineRecipe(Api.INSTANCE.blocks().blockSecurity.stack(1), ExtraUtils.bedrockium, WurmTweaksItems.ingotRainbowSteel, Api.INSTANCE.items().itemCell16k.stack(1), Api.INSTANCE.blocks().blockChest);
 		}
 }
