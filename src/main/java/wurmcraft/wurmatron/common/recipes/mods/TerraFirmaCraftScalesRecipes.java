704532aa41662613518099b8ef7d4b5778138606
Ficisism <tristenc12@gmail.com>
2016-06-06 21:40:05 -0400

    Actually Added TFCScales Recipes

---
+++ src/main/java/wurmcraft/wurmatron/common/recipes/mods/TerraFirmaCraftScalesRecipes.java
@@ -0,0 +1,9 @@
+package wurmcraft.wurmatron.common.recipes.mods;
+
+import weightingscales.WSRecipes;
+
+public class TerraFirmaCraftScalesRecipes {
+		public static void addRecipes() {
+				WSRecipes.registerRecipes();
+		}
+}
